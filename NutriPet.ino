99390a3c1b16dc5a584302b928dd73dc24470153
eduzinx <eduardoduarteney@gmail.com>
2026-05-30 14:46:24 -0300

    Add pause and release functionality to NutriPet

--- NutriPet.ino
+++ NutriPet.ino
@@ -3,7 +3,7 @@
 #include <LiquidCrystal_I2C.h>
 
 const byte SERVO=6, MAIS=4, MENOS=5, SET=2, RESET=3,
-TRINTAMAIS=7, TRINTAMENOS=8;
+TRINTAMAIS=7, TRINTAMENOS=8, LIBERAR=9, PAUSA=10;
 
 //configurando
 int minutoConfig = 0;
@@ -14,7 +14,8 @@ int minutoAtual = 0;
 int segundoAtual = 0;
 
 bool iniciado = false;
-
+bool pausado = false;
+bool cliquePausa = false;
 String modo;
 
 
@@ -30,8 +31,9 @@ void setup()
   pinMode(MENOS, INPUT_PULLUP);
   pinMode(SET, INPUT_PULLUP);
   pinMode(RESET, INPUT_PULLUP);
+  pinMode(LIBERAR, INPUT_PULLUP);
+  pinMode(PAUSA, INPUT_PULLUP);
   
-  attachInterrupt(0, ativar, FALLING);
 
   motor.attach(SERVO);
 
@@ -46,10 +48,28 @@ void setup()
 
 void loop()
 {
-  delay(100);
+  delay(1000);
   
+  if(digitalRead(SET)==0 && !iniciado){
+
+   if(horaAtual == 0 && minutoAtual == 0){
+
+      tela.clear();
+      tela.setCursor(0,0);
+      tela.print("Defina tempo!");
+   }
+   else{
+
+      iniciado = true;
+
+      horaConfig = horaAtual;
+      minutoConfig = minutoAtual;
+   }
+
+   delay(300);
+}
   
-  if(iniciado){
+  if(iniciado && !pausado){
     //Execução do timer e verificação da hora de liberação
     if(segundoAtual == -1){
       minutoAtual--;
@@ -97,7 +117,11 @@ void loop()
     trintaMenos();
   }
   
+  liberar();
+  
   reconfigurar();
+  
+  pausar();
 }
 
 /////////////////////////////////////
@@ -131,14 +155,19 @@ void amostradinho(){
     tela.print(segundoAtual);
   }
   
-  if(iniciado){
+  if(iniciado && !pausado){
      tela.setCursor(0,1);
           
      if(modo != "Timer rodando"){
         modo = "Timer rodando";
         tela.print(modo);
      }
-  } else {
+  } 
+  
+  
+  
+  
+  else {
      tela.setCursor(0,1);
            
      if(modo != "Configurando"){
@@ -165,13 +194,20 @@ void horaMenos() {
 
 void trintaMais() {
   if(digitalRead(TRINTAMAIS)==0 ){
-  	minutoAtual = minutoAtual + 30;
-    
-    if(minutoAtual == 60) {
-      horaAtual++;
-      minutoAtual = 0;
+
+    if(horaAtual == 23 && minutoAtual >= 30){
+      horaAtual = 23;
+      minutoAtual = 59;
     }
-    
+    else{
+      minutoAtual += 30;
+
+      if(minutoAtual >= 60){
+        minutoAtual -= 60;
+        horaAtual++;
+      }
+    }
+
     amostradinho();
   }
 }
@@ -193,6 +229,7 @@ void trintaMenos() {
 
 
 void ativar(){
+  
   iniciado = true;
   
   horaConfig = horaAtual;
@@ -212,5 +249,40 @@ void reconfigurar(){
   horaAtual = 0;
   minutoAtual = 0;
   segundoAtual = 0;
+  }
+}
+
+void liberar(){
+  if(digitalRead(LIBERAR)==0){
+  	if(horaAtual >= 0 && minutoAtual >= 0 && segundoAtual >= 0){
+
+  motor.write(180);
+
+  tela.clear();
+  tela.setCursor(0,0);
+  tela.print("Hora do rango!");
+
+  Serial.println("Hora do rango!");
+
+  delay(3000);
+
+  motor.write(0);
+
+  horaAtual = horaConfig;
+  minutoAtual = minutoConfig;
+  segundoAtual = 0;
+
+  tela.clear();
+	}
+  }
+}
+
+void pausar(){
+  if(digitalRead(PAUSA)==0){
+    cliquePausa = true;
+   pausado = !pausado;
+  }
+  if(digitalRead(PAUSA)==1){
+  cliquePausa=false;
   }
 }
